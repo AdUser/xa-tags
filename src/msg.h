3120287e82f24f88300763f0537c5548fe3a7258
Alex 'AdUser' Z <ad_user@lavabit.com>
2012-06-14 22:34:12 +1100

    + added error messages for memory management

--- src/msg.h
+++ src/msg.h
@@ -1,1 +1,3 @@
-#define MSG_M_OOM        _("%s:%i -- Can't allocate memory")
+#define MSG_M_OOM        _("%s:%i -- Can't allocate memory.\n")
+#define MSG_M_NULLPTR    _("%s:%i -- Unexpected NULL pointer.\n")
+#define MSG_M_REALLOC    _("%s:%i -- realoc() failed.\n")
