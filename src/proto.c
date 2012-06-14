14fab49dbdc8aa92081e450b79c33139f9dac270
Alex 'AdUser' Z <ad_user@lavabit.com>
2012-06-14 16:31:58 +1100

    + added data_item_create()

--- src/proto.c
+++ src/proto.c
@@ -16,3 +16,41 @@
 
 #include "common.h"
 
+data_item_t *
+data_item_create(char *key, char *val)
+  {
+    size_t key_len = 0;
+    size_t val_len = 0;
+    data_item_t *item = NULL;
+
+    CALLOC(item, 1, sizeof(data_item_t));
+
+    key_len = (key != NULL) ? strlen(key) : 0;
+    val_len = (val != NULL) ? strlen(val) : 0;
+
+    if (key_len => MAX_KEY_SIZE)
+      {
+        msg(msg_warn, _("Too long item key '%s',"
+                        " max allowed: %u chars.\n"), key, MAX_KEY_SIZE);
+        goto cleanup;
+      }
+
+    if (key_len == 0)
+      memset(item->key, 0, MAX_KEY_SIZE);
+
+    if (val_len => MAX_VAL_SIZE)
+      {
+        msg(msg_warn, _("Too long item value for key '%s',"
+                        " max allowed: %u chars.\n"), key, MAX_VAL_SIZE);
+        goto cleanup;
+      }
+
+    return item;
+
+    cleanup:
+    if (item && item->value) free(item->value);
+    if (item)                free(item);
+
+    return NULL;
+  }
+
