717124f611b15727c67e08a637339e88ea191000
Alex 'AdUser' Z <ad_user@lavabit.com>
2012-07-14 03:13:30 +1100

    + added new includes in common.h

--- src/common.h
+++ src/common.h
@@ -1,7 +1,10 @@
+#include <ctype.h>
 #include <stdarg.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #ifdef HAVE_GETTEXT
