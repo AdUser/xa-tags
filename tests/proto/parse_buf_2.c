bd46420eb69db175763863f56e99182c36492f97
Alex 'AdUser' Z <ad_user@lavabit.com>
2012-07-17 14:04:59 +0400

    + added testing cases for parse_buf()

---
+++ tests/proto/parse_buf_2.c
@@ -0,0 +1,45 @@
+#include "../tests.h"
+
+int main()
+{
+  conn_t *conn = NULL;
+  ipc_req_t *req = NULL;
+  char *buf = NULL;
+  int ret = 0;
+
+  SIGCATCH_INIT
+
+  CALLOC(conn, 1, sizeof(conn_t));
+
+  /* requests with operations */
+  /* 1: fully valid request */
+  CALLOC(req, 1, sizeof(ipc_req_t));
+  buf = "DB STAT\n";
+
+  ret = parse_buf(conn, req, buf, strlen(buf));
+  assert(ret == 0);
+  assert(conn->rd_buf_len == 0);
+  assert(req->type == REQ_DB && req->op == OP_D_STAT);
+  FREE(req);
+
+  /* 2: incorrect operation */
+  CALLOC(req, 1, sizeof(ipc_req_t));
+  buf = "DB TEST\n";
+
+  ret = parse_buf(conn, req, buf, strlen(buf));
+  assert(ret == 2);
+  assert(conn->rd_buf_len == 0);
+
+  /* 3: valid operation without '\n' */
+  buf = "DB \t\t\t CHECK";
+  ret = parse_buf(conn, req, buf, strlen(buf));
+  assert(ret == 1);
+
+  ret = parse_buf(conn, req, "\n", 1);
+  assert(ret == 0);
+  assert(conn->rd_buf_len == 0);
+  assert(req->type == REQ_DB && req->op == OP_D_CHECK);
+  FREE(req);
+
+  return 0;
+}
