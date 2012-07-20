bee35d9e3f85a71aad999ed56af5f4685ab78246
Alex 'AdUser' Z <ad_user@lavabit.com>
2012-07-20 17:12:37 +0400

    + added test cases for validate_data()

--- tests/proto/validate_data.c
+++ tests/proto/validate_data.c
@@ -73,6 +73,21 @@ TWO-LINE TEXT\n";
   FREE(data);
 
   /* DATA_M_UUID_TAGS */
+  CALLOC(data, 1, sizeof(data_t));
+  data->type = DATA_M_UUID_TAGS;
+  test =
+#include "test_map_of_uuid_and_tags.inc"
+
+  data->len = strlen(test) + 1;
+  STRNDUP(data->buf, test, 4096);
+  for (i = 0; i < data->len; i++)
+    if (data->buf[i] == '\n')
+      data->buf[i] = '\0';
+
+  ret = validate_data(data);
+  assert(ret == 0);
+  assert(data->items == 6);
+  FREE(data);
 
   return 0;
 }
