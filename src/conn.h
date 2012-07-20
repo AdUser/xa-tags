724c8bf7d520e53b625601ee3dd7b6531c8b48a0
Alex 'AdUser' Z <ad_user@lavabit.com>
2012-07-20 20:31:36 +0400

    + added fields to conn_t

--- src/conn.h
+++ src/conn.h
@@ -1,3 +1,6 @@
+/** connection flags */
+#define CONN_F_HUMAN     1 <<  0 /* enable b/j and hookers */
+
 typedef struct conn_t
   {
     int fd;
@@ -6,4 +9,6 @@ typedef struct conn_t
     char        *wr_buf;
     unsigned int rd_buf_len;
     unsigned int wr_buf_len;
+    uint16_t flags;
+    data_t errors;
   } conn_t;
