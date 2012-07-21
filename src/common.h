db5dc11456a13f6c0064dd482e450352909470f3
Alex 'AdUser' Z <ad_user@lavabit.com>
2012-07-21 17:20:42 +0400

    + added more system headers in common.h

--- src/common.h
+++ src/common.h
@@ -1,11 +1,17 @@
 #include <ctype.h>
 #include <stdarg.h>
 #include <stdbool.h>
+#include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
 
 #ifdef HAVE_GETTEXT
   #include <libintl.h>
