0fcc46f79605de5edd28631959c8d4b6e1802367
Alex 'AdUser' Z <ad_user@lavabit.com>
2012-07-26 12:30:01 +0400

    + added comment in buf.h

--- src/buf.h
+++ src/buf.h
@@ -1,8 +1,12 @@
+/** 'buf' should contain null-terminated string
+  * 'len' should contain strlen of 'buf', but without null-byte
+  */
 typedef struct buf_t
 {
   size_t len;
   char *buf;
 } buf_t;
 
+/** API functions */
 void buf_extend(buf_t *buf, char *str, size_t str_len);
 void buf_reduce(buf_t *buf, size_t len);
