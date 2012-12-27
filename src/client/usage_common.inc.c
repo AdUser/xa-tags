d31a5b5e5fe8cd2394313412e988c0bba6137e57
Alex 'AdUser' Z <ad_user@lavabit.com>
2012-12-27 23:49:33 +1100

    * updated usage()

--- src/client/usage_common.inc.c
+++ src/client/usage_common.inc.c
@@ -1,5 +1,6 @@
   fprintf(stdout,
+  "Common options:\n"
   "  -h          This help.\n"
   "  -v          Be more verbose.\n"
   "  -q          Be less verbose.\n"
-  "");
+  "\n");
--- src/client/usage_search.inc.c
+++ src/client/usage_search.inc.c
@@ -1,3 +1,6 @@
   fprintf(stdout,
   "  -F <str>    Search for file(s) with given substring in path.\n"
+#ifdef UNIQ_TAGS_LIST
+  "  -T <str>    Search for existing tag (for complete list - use '*').\n"
+#endif
   "");
