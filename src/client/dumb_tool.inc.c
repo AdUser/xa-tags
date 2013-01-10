a4b3ecb37ad3e897dc5237ce9b3ac0ce5f0f4d96
Alex 'AdUser' Z <ad_user@lavabit.com>
2013-01-10 20:27:52 +1100

    - generally simplified _handle_search_by_tag()

--- src/client/dumb_tool.inc.c
+++ src/client/dumb_tool.inc.c
@@ -92,60 +92,28 @@ _handle_tag_lst(const char *path, const char *unused)
 void
 _handle_search_by_tag(const char *path, const char *str)
 {
+  data_t file_tags;
   data_t search_tags;
   char *tmp = NULL;
-  char *buf = NULL;
-  size_t buf_size = 1024;
-  ssize_t attr_size = 0;
-  bool match = false;
-  const int fts_flags = FTS_PHYSICAL | FTS_NOCHDIR;
-  FTS *fts = NULL;
-  FTSENT *ftsent = NULL;
-  char *fts_argv[2];
+  bool match = true;
 
+  memset(&file_tags, 0, sizeof(data_t));
+
+  if (file_tags_get(path, &file_tags) > 0)
+    return;
+
+  data_items_merge(&file_tags, ' ');
   memset(&search_tags, 0, sizeof(data_t));
-  CALLOC(buf, 1, buf_size * sizeof(char));
-
   data_parse_tags(&search_tags, str);
 
-  fts_argv[0] = (char * const) path;
-  fts_argv[1] = NULL;
+  for (tmp = NULL; data_items_walk(&search_tags, &tmp) > 0; )
+    if (strstr(file_tags.buf, tmp) == NULL)
+      match = false;
 
-  if ((fts = fts_open(fts_argv, fts_flags, NULL)) == NULL)
-    msg(msg_error, MSG_F_FAILOPEN, path);
+  if (match)
+    _handle_tag_lst(path, NULL);
 
-  while ((ftsent = fts_read(fts)) != NULL)
-    if (ftsent->fts_info & (FTS_DP | FTS_D) ||
-        ftsent->fts_info & FTS_F)
-      {
-        /* not a function, because we can reduce a lot of calls *
-         * of malloc() and free() with one lazy growing 'buf'   */
-        errno = 0;
-        attr_size = getxattr(ftsent->fts_path, XATTR_TAGS, buf, buf_size - 1);
-        while (attr_size < 0 && errno == ERANGE)
-          {
-            buf_size *= 2;
-            tmp = buf;
-            REALLOC(buf, tmp, buf_size);
-            errno = 0;
-            attr_size = getxattr(ftsent->fts_path, XATTR_TAGS, buf, buf_size - 1);
-          }
-        buf[attr_size + 1] = '\0';
-        if (errno == 0)
-          {
-            match = true;
-            tmp = NULL;
-            while (data_items_walk(&search_tags, &tmp) > 0)
-              if (strstr(buf, tmp) == NULL)
-                match = false;
-
-            if (match)
-              _handle_tag_lst((ftsent->fts_path), NULL);
-          }
-      }
-
-  fts_close(fts);
-  free(buf);
+  data_clear(&file_tags);
   data_clear(&search_tags);
 }
 
