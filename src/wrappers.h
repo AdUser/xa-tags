df98fcf9fc492946dab486a90fe75c92c4ed1165
Alex 'AdUser' Z <ad_user@lavabit.com>
2013-02-10 20:40:50 +1100

    * rewritten wrapper macroses to use ASSERT() for message output

--- src/wrappers.h
+++ src/wrappers.h
@@ -1,22 +1,20 @@
-#define CALLOC(ptr, nmemb, size) \
-  if (((ptr) = calloc((nmemb), (size))) == NULL) \
-    msg(msg_error, MSG_M_OOM, __FILE__, __LINE__)
-    /* yes, i know about assert() */
-
-#define REALLOC(new_ptr, old_ptr, new_size) \
-  if (((new_ptr) = realloc((old_ptr), (new_size))) == NULL) \
-    msg(msg_error, MSG_M_REALLOC, __FILE__, __LINE__)
+#ifdef DEBUG
+  #define ASSERT(x, y) \
+    if (!(x)) msg(msg_error, COMMON_AST_FMT, __FILE__, __LINE__, (y))
+#else
+  #define ASSERT(x, y) (x)
+#endif
 
 #define FREE(ptr) \
   if (ptr != NULL) free((ptr)); \
   (ptr) = NULL
 
+#define CALLOC(ptr, nmemb, size) \
+  ASSERT((ptr) = calloc((nmemb), (size)), MSG_M_OOM)
+
+#define REALLOC(new_ptr, old_ptr, new_size) \
+  ASSERT((new_ptr) = realloc((old_ptr), (new_size)), MSG_M_REALLOC)
+
 #define STRNDUP(ptr, str, len) \
   ASSERT((((ptr) = strndup((str), (len))) != NULL), MSG_M_OOM)
 
-#ifdef DEBUG
-  #define ASSERT(x, y) \
-    if (!(x)) msg(msg_error, (y), __FILE__, __LINE__)
-#else
-  #define ASSERT(x, y) (x)
-#endif
