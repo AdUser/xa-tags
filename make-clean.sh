c287b7a6c421e57335f7ea129ed99e12e5468f7d
Alex 'AdUser' Z <ad_user@lavabit.com>
2013-02-10 20:47:38 +1100

    + added basic l10n support in build system (untested)

--- make-clean.sh
+++ make-clean.sh
@@ -5,9 +5,12 @@ make clean
 rm -f *.cmake
 rm -f CMakeCache.txt
 rm -f DartConfiguration.tcl
+rm -f ./po/*.mo
+rm -f ./po/*.pot
 
 find ./ -type d -a -name Testing    -exec rm -rf {} +
 find ./ -type d -a -name CMakeFiles -exec rm -rf {} +
 find ./ -type f -a -name Makefile   -exec rm     {} +
 find ./tests/ -type f -a -name *.cmake    -exec rm     {} +
 find ./src/   -type f -a -name *.cmake    -exec rm     {} +
+find ./po/    -type f -a -name *.cmake    -exec rm     {} +
