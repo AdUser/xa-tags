abc9af66f957e84872c36f50a604de70303787b6
Alex 'AdUser' Z <ad_user@lavabit.com>
2013-05-03 18:59:05 +1100

    * updated test for file_tags_set()

--- tests/file/file_tags_set.c
+++ tests/file/file_tags_set.c
@@ -2,6 +2,7 @@
 
 int main()
 {
+  int ret = 0;
   list_t tags;
   char buf[1024];
 
@@ -16,8 +17,9 @@ int main()
   list_item_add(&tags, "tag3", 0);
 
   assert(file_tags_set(TEST_FILE, &tags) == 0);
-  assert(getxattr(TEST_FILE, XATTR_TAGS, buf, 1024) > 0);
-  assert(memcmp(buf, "tag1 tag2 tag3\0", 15) == 0);
+  ret = getxattr(TEST_FILE, XATTR_TAGS, buf, 1024);
+  assert(ret == 14);
+  assert(memcmp(buf, "tag1 tag2 tag3\0", 14) == 0);
 
   unlink(TEST_FILE);
 
