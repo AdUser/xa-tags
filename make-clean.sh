1c2d2f53156551558e95e2c3abd20ea820d1b64b
Alex 'AdUser' Z <ad_user@lavabit.com>
2013-05-15 22:14:29 +1100

    * updated buildsystem

--- make-clean.sh
+++ make-clean.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 make clean
 
@@ -8,6 +8,13 @@ rm -f DartConfiguration.tcl
 rm -f ./po/*.mo
 rm -f ./po/*.pot
 rm -f ./tests/scripts/*.test.out
+rm -f debian/*.log
+rm -f debian/*.substvars
+rm -f debian/xa-tags-embedded/ -r
+rm -f debian/xa-tags/ -r
+rm -f debian/files
+rm -f debian/tmp/ -r
+rm -f obj*/ -r
 
 find ./ -type d -a -name Testing    -exec rm -rf {} +
 find ./ -type d -a -name CMakeFiles -exec rm -rf {} +
